122702f690247cded2f6a00279985e95a4ce4650
andrey <andr435@gmail.com>
2024-12-05 12:48:59 +0200

    update config

--- dcoya/app.py
+++ dcoya/app.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
 
     with app.app_context():
         set_routes()
-        app.run()
+        app.run(host='0.0.0.0', port=8000, use_reloader=True)
--- dcoya/config.py
+++ dcoya/config.py
@@ -1,6 +1,6 @@
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://dcoya:dcoya123@localhost/dcoya'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dcoya:dcoya123@postgres_container/dcoya'
     SECRET_KEY = 'app_secret_key'
     JWT_SECRET_KEY = 'jwt_secret_key'
     JWT_TOKEN_LOCATION = ['headers']
